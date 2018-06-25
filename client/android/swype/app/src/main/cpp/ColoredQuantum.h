0142898b74043faa1bea19c434cbe0d695a5cea7
babay88 <babay@mail.ru>
2018-06-26 02:04:13 +0300

    added increasing DD before color quantum

--- client/android/swype/app/src/main/cpp/ColoredQuantum.h
+++ client/android/swype/app/src/main/cpp/ColoredQuantum.h
@@ -36,6 +36,7 @@ public:
     };
 
     void coloredQuantumToSingleByte(uint32_t *argb, unsigned char *target, int width, int height) {
+        increaseContrast(argb, width, height);
         int size = width * height;
         int r, g, b;
 
@@ -59,7 +60,61 @@ public:
         }
     }
 
+    void increaseContrast(uint32_t *argb, int width, int height) {
+        int size = width * height;
+        unsigned char minR = 0xFF, maxR = 0,
+                minG = 0xFF, maxG = 0, minB = 0xFF, maxB = 0,
+                r, g, b;
+
+        for (int i = 0; i < size; ++i) {
+            uint32_t value = argb[i];
+            r = static_cast<unsigned char>((value & 0xff0000) >> 16);
+            g = static_cast<unsigned char>((value & 0xff00) >> 8);
+            b = static_cast<unsigned char>(value & 0xff);
+
+            if (r < minR)
+                minR = r;
+            if (r > maxR)
+                maxR = r;
+            if (g < minG)
+                minG = g;
+            if (g > maxG)
+                maxG = g;
+            if (b < minB)
+                minB = b;
+            if (b > maxB)
+                maxB = b;
+        }
+
+        float mulR = colorMul(minR, maxR);
+        float mulG = colorMul(minG, maxG);
+        float mulB = colorMul(minB, maxB);
+
+        for (int i = 0; i < size; ++i) {
+            uint32_t value = argb[i];
+            r = static_cast<unsigned char>((value & 0xff0000) >> 16);
+            g = static_cast<unsigned char>((value & 0xff00) >> 8);
+            b = static_cast<unsigned char>(value & 0xff);
+
+            r = static_cast<unsigned char>((r - minR) * mulR);
+            g = static_cast<unsigned char>((g - minG) * mulG);
+            b = static_cast<unsigned char>((b - minB) * mulB);
+
+            argb[i] = 0xFF000000 | r << 16 | g << 8 | b;
+        }
+    }
+
 private:
+
+    inline float colorMul(unsigned char &min, unsigned char &max) {
+        if (max > min) {
+            return 255.0f / (max - min);
+        } else {
+            min = 0;
+            max = 0xff;
+            return 1;
+        }
+    }
     unsigned int rgbBufferSize = 0;
     uint32_t *_rgbBuffer = NULL;
 
