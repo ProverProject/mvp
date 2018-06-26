527ebb3c69e34cb2a6d3e230195578cf53a31c2c
KrKrainov <k.a.krainov@gmail.com>
2018-06-26 11:15:33 +0300

    changed parameters chmod

--- server/http/restoringOrder.sh
+++ server/http/restoringOrder.sh
@@ -2,6 +2,6 @@
 chmod a+w "`dirname $0`/libs/phpqrcode"
 if [ -d "`dirname $0`/pdf" ]
 then
-    chmod a+w "`dirname $0`/pdf"
+    chmod 777 "`dirname $0`/pdf"
     find "`dirname $0`/pdf" -mindepth 1 -maxdepth 1 -type d -ctime +1 -exec rm -rf {} \;
 fi
